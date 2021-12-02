b95b5271e297f3a33e18723bd26600f005aefe2a
Lyndon Maydwell <lyndon@sordina.net>
2021-12-03 08:53:02 +1100

    day 2

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 .PHONY: all
-all: day1 day1b
+all: day1 day1b day2
 
-.PHONY: day1 echo
+.PHONY: day1
 day1:
 	cabal run advent2021 1 < data/day1.input
 	@echo
 
-.PHONY: day1b echo
+.PHONY: day1b
 day1b:
 	cabal run advent2021 1b < data/day1.input
+	@echo
+
+.PHONY: day2
+day2:
+	cabal run advent2021 2 < data/day2.input
 	@echo
--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import           System.Environment (getArgs)
 
 import qualified Advent01  (day1)
 import qualified Advent01b (day1b)
+import qualified Advent02  (day2)
 
 interactShow :: Show a => (String -> a) -> IO ()
 interactShow f = interact (show . f)
@@ -15,6 +16,7 @@ days :: [(String, IO ())]
 days =
     [ ("1",  interactShow Advent01.day1)
     , ("1b", interactShow Advent01b.day1b)
+    , ("2",  interactShow Advent02.day2)
     ]
 
 help :: a
