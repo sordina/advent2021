20d6f55dffb308c0272b6610b5b04eb0c9c7e245
Lyndon Maydwell <lyndon@sordina.net>
2022-05-26 16:36:17 +1000

    22a

--- src/Advent22.hs
+++ src/Advent22.hs
@@ -20,6 +20,7 @@ on x=10..12,y=10..12,z=10..12
 on x=11..13,y=11..13,z=11..13
 off x=9..11,y=9..11,z=9..11
 on x=10..10,y=10..10,z=10..10
+
 The first step (on x=10..12,y=10..12,z=10..12) turns on a 3x3x3 cuboid consisting of 27 cubes:
 
 10,10,10
@@ -49,6 +50,7 @@ The first step (on x=10..12,y=10..12,z=10..12) turns on a 3x3x3 cuboid consistin
 12,12,10
 12,12,11
 12,12,12
+
 The second step (on x=11..13,y=11..13,z=11..13) turns on a 3x3x3 cuboid that overlaps with the first. As a result, only 19 additional cubes turn on; the rest are already on from the previous step:
 
 11,11,13
@@ -70,6 +72,7 @@ The second step (on x=11..13,y=11..13,z=11..13) turns on a 3x3x3 cuboid that ove
 13,13,11
 13,13,12
 13,13,13
+
 The third step (off x=9..11,y=9..11,z=9..11) turns off a 3x3x3 cuboid that overlaps partially with some cubes that are on, ultimately turning off 8 cubes:
 
 10,10,10
@@ -80,6 +83,7 @@ The third step (off x=9..11,y=9..11,z=9..11) turns off a 3x3x3 cuboid that overl
 11,10,11
 11,11,10
 11,11,11
+
 The final step (on x=10..10,y=10..10,z=10..10) turns on a single cube, 10,10,10. After this last step, 39 cubes are on.
 
 The initialization procedure only uses cubes that have x, y, and z positions of at least -50 and at most 50. For now, ignore cubes outside this region.
@@ -108,6 +112,7 @@ off x=18..30,y=-20..-8,z=-3..13
 on x=-41..9,y=-7..43,z=-33..15
 on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
 on x=967..23432,y=45373..81175,z=27513..53682
+
 The last two steps are fully outside the initialization procedure area; all other steps are fully within it. After executing these steps in the initialization procedure region, 590784 cubes are on.
 
 Execute the reboot steps. Afterward, considering only cubes in the region x=-50..50,y=-50..50,z=-50..50, how many cubes are on?
@@ -115,14 +120,24 @@ Execute the reboot steps. Afterward, considering only cubes in the region x=-50.
 -}
 
 import Text.RawString.QQ (r)
+import Data.List.Split ( splitOn )
+import Control.Arrow ((&&&))
+import Data.Monoid (Last (Last, getLast))
+import Data.Maybe (mapMaybe, fromMaybe)
 
 -- | Testing day22
 -- >>> day22 testInput
--- foo
+-- 39
+-- 
+-- >>> day22 testInput2
+-- 590784
 
 -- | Testing parseInput
 -- >>> parseInput testInput
--- bar
+-- [(True,((10,12),(10,12),(10,12))),(True,((11,13),(11,13),(11,13))),(False,((9,11),(9,11),(9,11))),(True,((10,10),(10,10),(10,10)))]
+-- 
+-- >>> parseInput testInput2
+-- [(True,((-20,26),(-36,17),(-47,7))),(True,((-20,33),(-21,23),(-26,28))),(True,((-22,28),(-29,23),(-38,16))),(True,((-46,7),(-6,46),(-50,-1))),(True,((-49,1),(-3,46),(-24,28))),(True,((2,47),(-22,22),(-23,27))),(True,((-27,23),(-28,26),(-21,29))),(True,((-39,5),(-6,47),(-3,44))),(True,((-30,21),(-8,43),(-13,34))),(True,((-22,26),(-27,20),(-29,19))),(False,((-48,-32),(26,41),(-47,-37))),(True,((-12,35),(6,50),(-50,-2))),(False,((-48,-32),(-32,-16),(-15,-5))),(True,((-18,26),(-33,15),(-7,46))),(False,((-40,-22),(-38,-28),(23,41))),(True,((-16,35),(-41,10),(-47,6))),(False,((-32,-23),(11,30),(-14,3))),(True,((-49,-5),(-3,45),(-29,18))),(False,((18,30),(-20,-8),(-3,13))),(True,((-41,9),(-7,43),(-33,15))),(True,((-54112,-39298),(-85059,-49293),(-27449,7877))),(True,((967,23432),(45373,81175),(27513,53682)))]
 
 testInput :: String
 testInput = unlines $ tail $ lines [r|
@@ -158,8 +173,38 @@ on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
 on x=967..23432,y=45373..81175,z=27513..53682
 |]
 
-day22 :: String -> Int
-day22 = undefined
+test :: (Int,Int,Int) -> (Bool, ((Int,Int),(Int,Int),(Int,Int))) -> Maybe Bool
+test (x,y,z) (b,(x',y',z')) | x <? x' && y <? y' && z <? z' = Just b
+test _ _ = Nothing
 
-parseInput :: String -> Int
-parseInput = undefined
+tests :: (Int,Int,Int) -> [(Bool, ((Int,Int),(Int,Int),(Int,Int)))] -> Bool
+tests p = fromMaybe False . getLast . mconcat . map (Last . test p)
+
+(<?) :: Int -> (Int,Int) -> Bool
+p <? (l,h) = p >= l && p <= h
+
+day22 :: String -> Int
+day22 i = length $ filter id $ map (`tests` parsed) cs
+    where
+    r = [-50..50]
+    cs = [(x,y,z) | x <- r, y <- r, z <- r]
+    parsed = parseInput i
+
+parseInput :: String -> [(Bool, ((Int,Int),(Int,Int),(Int,Int)))]
+parseInput = map parseLine . filter (not . null) . lines
+    where
+    parseLine = (flag . head &&& coords . last) . words
+
+    flag "on" = True
+    flag "off" = False
+    flag _ = error "Partial flag"
+
+    coords = coord . map parseWord . splitOn ","
+
+    coord [('x',(xl,xh)), ('y', (yl,yh)), ('z', (zl,zh))] = ((xl,xh), (yl,yh), (zl,zh))
+    coord _ = error "partial coord"
+
+    parseWord (a:'=':ns) = (a, parseNs ns)
+    parseWord _ = error "partial word"
+
+    parseNs ns = case splitOn ".." ns of [l,h] -> (read l, read h)
